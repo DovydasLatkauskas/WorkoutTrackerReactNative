9cc4ac1b391dd1574994516e4460b6764305b3d0
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-05-29 21:27:56 +0300

    initial commit

---
+++ app/CommonStyles.tsx
@@ -0,0 +1,9 @@
+import {StyleSheet} from "react-native";
+
+const commonStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+});
+export default commonStyles;
---
+++ app/screens/StartWorkoutScreen.tsx
@@ -0,0 +1,11 @@
+import {View} from "react-native";
+import commonStyles from "../CommonStyles";
+
+function StartWorkoutScreen(){
+    return(
+        <View style={commonStyles.container}>
+
+        </View>
+    )
+}
+export default StartWorkoutScreen;
