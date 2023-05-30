7337faef250a3e4658a2dad17cfe24bdd48b8b21
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-05-30 17:41:05 +0300

    added exercise screen

--- app/screens/StartWorkoutScreen.tsx
+++ app/screens/StartWorkoutScreen.tsx
@@ -1,11 +1,29 @@
-import {View} from "react-native";
+import {Alert, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import commonStyles from "../components/CommonStyles";
 
 function StartWorkoutScreen(){
-    return(
-        <View style={commonStyles.container}>
+    const handleAddExerciseButtonPress = () => Alert.alert(
+    "Add Exercise", "should take you to a screen to add an exercise",
+    [{text:"ok"}]) // temporary stand-in
 
-        </View>
+    return(
+        <SafeAreaView style={commonStyles.container, localStyles.container}>
+            <TouchableOpacity style={[commonStyles.button, localStyles.addExerciseButton]} onPress={handleAddExerciseButtonPress}>
+                <Text style={commonStyles.buttonText}>Add Exercise</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     )
 }
+
+const localStyles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex:1
+    },
+    addExerciseButton: {
+        height: 50,
+        width: "60%"
+    }
+});
 export default StartWorkoutScreen;
