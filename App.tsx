e29bc8106b0b5db886f5ef554ee117b0d3a348b1
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-05-31 10:38:06 +0300

    before changing navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from "./app/screens/HomeScreen";
 import AppNavigator from "./app/navigator";
+import {NavigationContainer} from "@react-navigation/native";
+import BottomTab from "./app/BottomTab";
 
 export default function App() {
   return (
-    <AppNavigator/>
+      <NavigationContainer>
+        <BottomTab/>
+      </NavigationContainer>
   );
 }
 
--- app/BottomTab.tsx
+++ app/BottomTab.tsx
@@ -14,3 +14,5 @@ function BottomTab(){
         </Tab.Navigator>
     )
 }
+
+export default BottomTab;
